283c9549ebbccd520127b8b650c8854ef1a10ac3
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-04-20 15:38:01 -0300

    Update Aulas.js

--- src/components/Aulas.js
+++ src/components/Aulas.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import aulas from '../Data'
 
+import { Link, useRouteMatch } from 'react-router-dom'
+
 export default function Aulas() {
+
+  const match = useRouteMatch()
+
+  const [url, setUrl] = useState("")
+
+  useEffect(() => {
+    // o último caracter do match.url é uma "/" ?
+    const newUrl = match.url.slice(-1) === "/" ? match.url : match.url + "/"
+    setUrl(newUrl)
+  }, [match])
+
   return (
       <div className="page">
           <h1>Aulas</h1>
           <ul className="aulas">
-            {aulas.map(aula => <li key={aula.id}>{aula.title}</li>)}
+            {aulas.map(aula => 
+              <li key={aula.id}>
+                <Link className="link" to={`${url}${aula.id}`}>{aula.title}</Link>
+              </li>
+            )}
           </ul>
       </div>
   )
