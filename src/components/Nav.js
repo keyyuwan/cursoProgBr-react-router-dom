dc28ae8feffe5c2d7acf12e918815b77618fdbd9
Key Yu Wan <keyflcbyuwan@gmail.com>
2021-04-27 19:04:42 -0300

    added private route and login/logout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link, useHistory, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
-import { Link } from 'react-router-dom'
+import AuthButton from './AuthButton'
 
 export default function Nav() {
+
+    const login = useSelector(state => state)
+
+    const history = useHistory() // objeto que gerencia a navegação da página
+    const location = useLocation()
+
+    useEffect(() => {
+        let { from } = location.state || {from: {pathname: "/"}}
+        if (login) {
+            history.replace(from)
+        }
+    }, [login])
+
     return (
         <div className="nav">
             <h1>
                 <Link to="/">ProgBR</Link>
             </h1>
             <ul className="nav-links">
+                <AuthButton />
                 <li>
                     <Link to="/aulas">Aulas</Link>
                 </li>
